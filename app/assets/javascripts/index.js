b7adfe08c39ed7390f382085162cc5124df6657d
CJOYBLUV <dave_hemmer@hotmail.com>
2015-07-31 01:21:49 -0700

    note new dynamic

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,157 +1,167 @@
 
 $(function() {
 
-  L.mapbox.accessToken = 'pk.eyJ1IjoiY2pveWJsdXYiLCJhIjoiNzQ3MWQzMjNiOGI2ZDQzOTJhNGFlY2YyMzZiMjg5NDIifQ.A6YxoDaBVo73wIutjHFrQg';
-  var map = L.mapbox.map('map', 'cjoybluv.n0012j3j')
-      .setView([47.613968, -122.335195], 12);
+  var mapDiv = $('#map');
+  if (typeof mapDiv !== 'undefined') {
 
-  // Initialize the geocoder control and add it to the map.
-  var geocoderControl = L.mapbox.geocoderControl('mapbox.places');
-  geocoderControl.addTo(map);
+    L.mapbox.accessToken = 'pk.eyJ1IjoiY2pveWJsdXYiLCJhIjoiNzQ3MWQzMjNiOGI2ZDQzOTJhNGFlY2YyMzZiMjg5NDIifQ.A6YxoDaBVo73wIutjHFrQg';
+      var map = L.mapbox.map('map', 'cjoybluv.n0012j3j')
+          .setView([47.613968, -122.335195], 12);
 
-  var markers = [];
+      // Initialize the geocoder control and add it to the map.
+      var geocoderControl = L.mapbox.geocoderControl('mapbox.places');
+      geocoderControl.addTo(map);
 
-  console.log('on load', gon.customers);
+      var markers = [];
 
-  // lat.lng  47.600 - 47.700     SEATTLE ZONE
-  //          -122.300 - -122.400
+      console.log('on load', gon.customers);
 
-  var pinCustomer = function(customer) {
-    // console.log(customer);
-    var lngLat = [customer.lnglat.split(',')[0],customer.lnglat.split(',')[1]];
-    var placeName = customer.address;
-    var placeTitle = customer.name;
-    var marker = L.mapbox.featureLayer({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: lngLat
-        },
-        properties: {
-            title: placeTitle,
-            description: placeName,
-            'marker-size': 'medium',
-            'marker-color': '#B70911'
-        }
-    }).addTo(map);
-    markers.push(marker);
-  };
+      // lat.lng  47.600 - 47.700     SEATTLE ZONE
+      //          -122.300 - -122.400
 
-  // LOAD MARKERS
-  for (var c=0;c<gon.customers.length;c++) {
-    var pin = pinCustomer(gon.customers[c]);
-  }
-  for (var n=0;n<gon.notes.length;n++) {
-    var customer = gon.customersAll.filter(function (customer) {
-      if (customer.id==gon.notes[n].customer_id) {
-        return true;
-      } else {
-        return false;
+      var pinCustomer = function(customer) {
+        // console.log(customer);
+        var lngLat = [customer.lnglat.split(',')[0],customer.lnglat.split(',')[1]];
+        var placeName = customer.address;
+        var placeTitle = customer.name;
+        var marker = L.mapbox.featureLayer({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: lngLat
+            },
+            properties: {
+                title: placeTitle,
+                description: placeName,
+                'marker-size': 'medium',
+                'marker-color': '#B70911'
+            }
+        }).addTo(map);
+        markers.push(marker);
+      };
+
+      // LOAD MARKERS
+      for (var c=0;c<gon.customers.length;c++) {
+        var pin = pinCustomer(gon.customers[c]);
       }
-    });
-    console.log('load note markers : customer',customer);
-    if (customer.length>0) {
-      var pin = pinCustomer(customer[0]);
-    }
-
-  }
-
-  // console.log('markers:',markers);
-
-  map.on('popupopen',function(centerMarker) {
-    var latAdj = (.017 * 12) / (map.getZoom() * 14);
-    map.panTo([centerMarker.popup._latlng.lat+latAdj,centerMarker.popup._latlng.lng]);
-    console.log(map.getZoom(),latAdj);
-  });
-
-
-  $('#mapButton').on('click', function(e) {
-    console.log('mapButton Clicked!',e,markers);
-    for (var c=0;c<gon.customers.length;c++) {
-      map.removeLayer(markers[c]);
-    }
-  });
-
-
-  $('.BD2Go-marker').on('click', function(e) {
-    var noteId = e.currentTarget.dataset.noteid;
-    console.log('BD2Go-marker:click : noteId',noteId);
-    console.log('BD2Go-marker:click : gon.customers',gon.customers);
-    var targetTitle = '';
-    if (typeof noteId !== 'undefined') {
-      console.log(gon.notes);
-      var note = gon.notes.filter(function (note) {
-        if (note.id == noteId) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-      var customerId = note[0].customer_id;
-      var customer = gon.customersAll.filter(function (customer) {
-        if (customer.id == customerId) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-      if (customer.length>0) {
-        targetTitle = customer[0].name;
-      }
-    } else {
-      var targetTitle = e.toElement.dataset.title;
-    }
-    if (targetTitle !== '') {
-      for (var m=0;m<markers.length;m++) {
-        if (markers[m]._geojson.properties.title==targetTitle) {
-          var latAdj = (.017 * 12) / (map.getZoom() * 14);
-          map.panTo([markers[m]._geojson.geometry.coordinates[1]+latAdj,markers[m]._geojson.geometry.coordinates[0]]);
-          markers[m].openPopup();
-        }
-      }
-    }
-  });
-
-  $('.completed-button').on('click', function(e) {
-    var noteId = e.currentTarget.dataset.id;
-    console.log('completed button clicked',notedId);
-  });
-
-
-
-  // Listen for the `found` result and display the first result
-  // in the output container. For all available events, see
-  // https://www.mapbox.com/mapbox.js/api/v2.2.1/l-mapbox-geocodercontrol/#section-geocodercontrol-on
-  geocoderControl.on('found', function(res) {
-
-      var lngLat = res.results.features[0]['center'];
-      var placeName = res.results.features[0]['place_name'];
-
-      // output.innerHTML = JSON.stringify(res.results.features[0]);
-      // output.innerHTML = ('lng,lat: '+lngLat+'    placeName: '+placeName);
-
-
-      var marker = L.mapbox.featureLayer({
-          // this feature is in the GeoJSON format: see geojson.org
-          // for the full specification
-          type: 'Feature',
-          geometry: {
-              type: 'Point',
-              // coordinates here are in longitude, latitude order because
-              // x, y is the standard for GeoJSON and many formats
-              coordinates: lngLat
-          },
-          properties: {
-              title: 'User geoCode Location',
-              description: placeName,
-              // one can customize markers by adding simplestyle properties
-              // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-              'marker-size': 'medium',
-              'marker-color': '#1DAD23',
-              'marker-symbol': 'bar'
+      for (var n=0;n<gon.notes.length;n++) {
+        var customer = gon.customersAll.filter(function (customer) {
+          if (customer.id==gon.notes[n].customer_id) {
+            return true;
+          } else {
+            return false;
           }
-      }).addTo(map);
-  });
-  // end of geoCoder.FOUND >> pin it
+        });
+        console.log('load note markers : customer',customer);
+        if (customer.length>0) {
+          var pin = pinCustomer(customer[0]);
+        }
+
+      }
+
+      // console.log('markers:',markers);
+
+      map.on('popupopen',function(centerMarker) {
+        var latAdj = (.017 * 12) / (map.getZoom() * 14);
+        map.panTo([centerMarker.popup._latlng.lat+latAdj,centerMarker.popup._latlng.lng]);
+        console.log(map.getZoom(),latAdj);
+      });
+
+
+      $('#mapButton').on('click', function(e) {
+        console.log('mapButton Clicked!',e,markers);
+        for (var c=0;c<gon.customers.length;c++) {
+          map.removeLayer(markers[c]);
+        }
+      });
+
+
+      $('.BD2Go-marker').on('click', function(e) {
+        var noteId = e.currentTarget.dataset.noteid;
+        console.log('BD2Go-marker:click : noteId',noteId);
+        console.log('BD2Go-marker:click : gon.customers',gon.customers);
+        var targetTitle = '';
+        if (typeof noteId !== 'undefined') {
+          console.log(gon.notes);
+          var note = gon.notes.filter(function (note) {
+            if (note.id == noteId) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+          var customerId = note[0].customer_id;
+          var customer = gon.customersAll.filter(function (customer) {
+            if (customer.id == customerId) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+          if (customer.length>0) {
+            targetTitle = customer[0].name;
+          }
+        } else {
+          var targetTitle = e.toElement.dataset.title;
+        }
+        if (targetTitle !== '') {
+          for (var m=0;m<markers.length;m++) {
+            if (markers[m]._geojson.properties.title==targetTitle) {
+              var latAdj = (.017 * 12) / (map.getZoom() * 14);
+              map.panTo([markers[m]._geojson.geometry.coordinates[1]+latAdj,markers[m]._geojson.geometry.coordinates[0]]);
+              markers[m].openPopup();
+            }
+          }
+        }
+      });
+
+      $('.completed-button').on('click', function(e) {
+        var noteId = e.currentTarget.dataset.id;
+        console.log('completed button clicked',notedId);
+      });
+
+
+
+      // Listen for the `found` result and display the first result
+      // in the output container. For all available events, see
+      // https://www.mapbox.com/mapbox.js/api/v2.2.1/l-mapbox-geocodercontrol/#section-geocodercontrol-on
+      geocoderControl.on('found', function(res) {
+
+          var lngLat = res.results.features[0]['center'];
+          var placeName = res.results.features[0]['place_name'];
+
+          // output.innerHTML = JSON.stringify(res.results.features[0]);
+          // output.innerHTML = ('lng,lat: '+lngLat+'    placeName: '+placeName);
+
+
+          var marker = L.mapbox.featureLayer({
+              // this feature is in the GeoJSON format: see geojson.org
+              // for the full specification
+              type: 'Feature',
+              geometry: {
+                  type: 'Point',
+                  // coordinates here are in longitude, latitude order because
+                  // x, y is the standard for GeoJSON and many formats
+                  coordinates: lngLat
+              },
+              properties: {
+                  title: 'User geoCode Location',
+                  description: placeName,
+                  // one can customize markers by adding simplestyle properties
+                  // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+                  'marker-size': 'medium',
+                  'marker-color': '#1DAD23',
+                  'marker-symbol': 'bar'
+              }
+          }).addTo(map);
+      });
+      // end of geoCoder.FOUND >> pin it
+
+
+
+  }
+
+
+
 
 });
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,10 +13,19 @@ class NotesController < ApplicationController
   end
 
   def create
-    # render json: params
-    n = Note.new(subject: params[:note][:subject], body: params[:note][:body])
-    n.due_date_time = params[:note][:due_date_time]
-    n.completed = params[:note][:completed]
+    # note = params[:note]
+    # task_date = Time.new(note['due_date_time(1i)'].to_i,note['due_date_time(2i)'].to_i,note['due_date_time(3i)'].to_i,0,0,0)
+    # render plain: note
+    note = params[:note]
+    n = Note.new(subject: note[:subject], body: note[:body])
+    due_date_time = note[:due_date_time]
+    n.completed = false
+    if (due_date_time == nil || due_date_time == '')
+      n.due_date_time = Time.new(note['due_date_time(1i)'].to_i,note['due_date_time(2i)'].to_i,note['due_date_time(3i)'].to_i,0,0,0)-(7*60*60)
+      n.completed = note[:completed]
+    else
+      n.due_date_time = note[:due_date_time]
+    end
     n.customer_id = params[:customer_id]
     n.user_id = session[:user_id]
     n.save
