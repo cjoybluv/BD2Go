0c8388d202564a6d4c5bd13abbce3be1c64915c0
CJOYBLUV <dave_hemmer@hotmail.com>
2015-07-30 17:44:03 -0700

    astro sign on new.user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,6 @@
 class MainController < ApplicationController
 
   def index
-    @users = User.all
     @customers = []
     @notes = []
     if (session[:user_id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
    private
 
   def user_params
-    params.require(:user).permit(:email,:password,:name)
+    params.require(:user).permit(:email,:password,:name, :astro_sign)
   end
 
 
